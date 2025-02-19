76e0e9bfeff634e581a4d9a2dbbe95c1a6f9a03b
Artur Opala <250927+arturopala@users.noreply.github.com>
2025-02-19 18:27:11 +0000

    add SCM info

--- project.scala
+++ project.scala
@@ -1,6 +1,6 @@
 //> using scala 3.5.2
 //> using jvm 21
-//> using dep org.scalameta::munit:1.1.0
+//> using test.dep org.scalameta::munit:1.1.0
 //> using option -Xmax-inlines 128
 //> using test.resourceDir ./test-resources
 //> using publish.organization "org.encalmo"
@@ -8,3 +8,4 @@
 //> using publish.url "https://github.com/encalmo/graphs"
 //> using publish.license "Apache-2.0"
 //> using publish.developer "arturopala|Artur Opala|https://github.com/arturopala"
+//> using publish.versionControl github:encalmo/graphs
